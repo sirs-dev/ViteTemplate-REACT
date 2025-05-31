7267844ab5c5f222cc708bd3df05745e8592a8d1
sirs-dev <srdev.santamaria@gmail.com>
2025-05-30 23:26:14 -0500

    Pequeño cambio para probar CI/CD

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" /> // Si usas TypeScript para que reconozca import.meta.env
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/setupTests.js'],
+  test: { // Configuración de Vitest
+    globals: true, // Para no tener que importar describe, it, expect, etc. en cada archivo de test
+    environment: 'jsdom', // Simular un entorno DOM
+    setupFiles: './src/setupTests.js', // (Opcional) Archivo para configuración global de pruebas
+    css: true, // Si tus componentes importan CSS y quieres que se procese (o usa mocks)
   },
 });
